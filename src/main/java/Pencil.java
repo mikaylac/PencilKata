265b174149d29e4fca8a97002e09697ae40708f1
Mikayla Clemons <mjclemons3@gmail.com>
2020-02-29 17:45:26 -0500

    002-Made pencil able to write to paper

--- src/main/java/Pencil.java
+++ src/main/java/Pencil.java
@@ -1,3 +1,8 @@
 public class Pencil {
 
+    public Paper paperToWriteOn;
+
+    public void write(String expectedStringToStore) {
+        this.paperToWriteOn.storeText(expectedStringToStore);
+    }
 }
--- src/test/java/PencilTest.java
+++ src/test/java/PencilTest.java
@@ -1,11 +1,21 @@
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class PencilTest {
 
     @Test
     public void shouldStoreStringOnPaperWhenWriteIsCalled(){
-        //will complete on next commit
+        Pencil pencil = new Pencil();
+        Paper paper = new Paper();
+        pencil.paperToWriteOn = paper;
+        String expectedStringToStore = "Bonjour";
+
+        pencil.write(expectedStringToStore);
+
+        String blankSpace = " ";
+        assertEquals(expectedStringToStore + blankSpace, paper.getText());
     }
 
 }
